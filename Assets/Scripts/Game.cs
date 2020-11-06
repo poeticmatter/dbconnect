779a7733f73f6c9323554821e4b0b6b62b98a784
Aurore <d00m.poet@gmail.com>
2020-11-06 22:27:19 +0200

    first commit

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+
+public class Game : MonoBehaviour
+{
+    public Text playerDisplay;
+    public Text scoreDisplay;
+
+	private void Awake()
+	{
+		if (!DBManager.LoggedIn)
+		{
+			SceneManager.LoadScene(0);
+		}
+		playerDisplay.text = "Player: " + DBManager.username;
+		scoreDisplay.text = "Score: " + DBManager.score;
+	}
+
+	public void CallSaveData()
+	{
+		StartCoroutine(SavePlayerData());
+
+	}
+
+	IEnumerator SavePlayerData()
+	{
+		WWWForm form = new WWWForm();
+		form.AddField("name", DBManager.username);
+		form.AddField("score", DBManager.score);
+
+		UnityWebRequest request = UnityWebRequest.Post("http://localhost/sqlconnect/savedata.php", form);
+		yield return request.SendWebRequest();
+
+		if (request.downloadHandler.text == "0")
+		{
+			Debug.Log("Game Saved");
+			UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+		}
+		else
+		{
+			Debug.Log("Save Failed. Error #" + request.downloadHandler.text);
+		}
+
+		DBManager.LogOut();
+	}
+
+	public void IncreaseScore()
+	{
+		DBManager.score++;
+		scoreDisplay.text = "Score: " + DBManager.score;
+	}
+}
---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+    public InputField nameField;
+    public InputField passwordField;
+
+    public Button submitButton;
+
+    public void CallLogin()
+    {
+        StartCoroutine(LoginPlayer());
+    }
+
+    IEnumerator LoginPlayer()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", nameField.text);
+        form.AddField("password", passwordField.text);
+
+        UnityWebRequest request = UnityWebRequest.Post("http://localhost/sqlconnect/Login.php", form);
+        yield return request.SendWebRequest();
+
+        if (request.downloadHandler.text[0] == '0')
+        {
+            Debug.Log("User logged in successfully");
+            DBManager.username = nameField.text;
+            DBManager.score = int.Parse(request.downloadHandler.text.Split('\t')[1]);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+        else
+        {
+            Debug.Log("User login failed. Error #" + request.downloadHandler.text);
+        }
+    }
+
+    public void VerifyInputs()
+    {
+        submitButton.interactable = (nameField.text.Length >= 8 && passwordField.text.Length >= 8);
+    }
+}
